7b2f3097baf89efb87f64a16b6476bc40e184e44
Nikita Prokopyev <nikpro200125@gmail.com>
2021-10-30 23:37:52 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,73 +1,87 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import './App.css';
+import Node from "./Node";
 
 // какие-то константы, яхз
-const buttons = ['Clear', '←', '^', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-const manips = ['Clear', '←', '^', '/', '*', '-', '=', '+'];
+const buttons = ['clear', '←', '^', '%', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+const manips = ['clear', '←', '^', '%', '*', '-', '=', '+'];
 let tabs = 1;
+
 // Пожилая кнопка
 const Button = (props) => {
-  if (manips.includes(props.name))
-    return (<button key={props.name} className="highlight" name={props.name} onClick={props.onClick} tabIndex={tabs++}>{props.name}</button>);
-  else
-    return (<button key={props.name} name={props.name} onClick={props.onClick}  tabIndex={tabs++}>{props.name}</button>);
+  if (manips.includes(props.name)) {
+    return (<button key={props.name} className="highlight" name={props.name} onClick={props.onClick}
+                    tabIndex={tabs++}>{props.name}</button>);
+  } else {
+    return (<button key={props.name} name={props.name} onClick={props.onClick} tabIndex={tabs++}>{props.name}</button>);
+  }
 }
 
 // приложение вот
 const App = () => {
   const [result, setResult] = useState('0');
-  useEffect(() => {
-    window.addEventListener('keyup', (event) => {
-      if (event.key.match(/^[0-9.+\-/^*]$/)) {
-        document.getElementsByName(event.key)[0].click();
-      } else {
-        switch (event.key) {
-          case 'Backspace' :
-            document.getElementsByName('←')[0].click();
-            break;
-          case 'Escape' :
-            document.getElementsByName('Clear')[0].click();
-            break;
-          // case 'Enter' :
-          //   document.getElementsByName('=')[0].click();
-          //   break;
-          case '=' :
-            document.getElementsByName(event.key)[0].click();
-            break;
-          // case 'ArrowLeft' : document.activeElement.previousElementSibling.focus();break;
-          // case 'ArrowRight' : document.activeElement.nextElementSibling.focus();break;
-          default :
-            console.log('Error : unknown button ' + event.key);
-        }
-      }
-    });
-  }, []);
 
-  const calc = () => {
-    if (result.at(result.length - 1).match(/^\d$/)) {
-      const Root = new node(result);
-      const r = Root.parse();
-      setResult(r.toString());
+  const validMiddleWare = (value) => value.match(/^[0-9./\-*+%^]+$/) && !value.match(/\+{2,}|-{2,}|%{2,}|\^{2,}|\.{2,}|\*{2,}|^\./) && checkDots2(value);
+
+  const e = (value) => {
+    if (value.charAt(value.length - 1) === '=') {
+      value = value.slice(0, -1);
+      if (validMiddleWare(value) && !value.match(/^0[0-9]+/) && !value.match(/%$|\+$|-$|\^$|\.$|\*$/)) {
+        setResult(new Node(value).parse().toString());
+      }
+    } else {
+      if (validMiddleWare(value)) {
+        if (!value.match(/^0[0-9]+/)) {
+          setResult(value);
+        } else {
+          setResult(value.slice(1));
+        }
+      } else if (value === '') {
+        setResult('0');
+      }
     }
   }
 
-  // точка есть? а если найду?!
+  const calc = () => {
+    if (result.charAt(result.length - 1).match(/^\d$/)) {
+      setResult(new Node(result).parse().toString());
+    }
+  }
+
+// точка есть? а если найду?!
   const checkDots = () => {
     let counter = 0;
     for (let i = result.length - 1; i > -1; i--) {
-      if (result[i].match(/^[+\-/*^]$/)) break;
-      if (result[i] === '.') counter++;
+      if (result[i].match(/^[+\-%*^]$/)) {
+        break;
+      }
+      if (result[i] === '.') {
+        counter++;
+      }
     }
     return counter === 0;
   }
 
-  // ручка вот, можешь что-нить написать
+  const checkDots2 = (value) => {
+    let counter = 0;
+    for (let i = value.length - 1; i > -1 && counter < 2; i--) {
+      if (value[i].match(/^[+\-%*^]$/)) {
+        counter = 0;
+      }
+      if (value[i] === '.') {
+        counter++;
+      }
+    }
+    return counter < 2;
+  }
+
+// ручка вот, можешь что-нить написать
   const handleClick = (e) => {
     const clickSymbol = e.target.name;
 
     if (!clickSymbol.match(/^[0-9]$/)) {
       switch (clickSymbol) {
-        case 'Clear' :
+        case 'clear' :
           setResult('0');
           break;
         case '←' :
@@ -80,7 +94,7 @@ const App = () => {
           calc();
           break;
         case '.' :
-          if (result.at(result.length - 1).match(/^\d$/) && checkDots(result))
+          if (result.charAt(result.length - 1).match(/^\d$/) && checkDots(result))
             setResult(result + clickSymbol);
           break;
         case '-' :
@@ -89,22 +103,24 @@ const App = () => {
             break;
           }
         default :
-          if (result.at(result.length - 1).match(/^\d$/)) {
+          if (result.charAt(result.length - 1).match(/^\d$/)) {
             setResult(result + clickSymbol);
-          } else if (!result.at(result.length - 1).match(/^\.$/))
+          } else if (!result.charAt(result.length - 1).match(/^\.$/)) {
             setResult(result.slice(0, -1) + clickSymbol);
+          }
       }
-    } else if (result === '0')
+    } else if (result === '0') {
       setResult(clickSymbol);
-    else
+    } else {
       setResult(result + clickSymbol);
+    }
   }
 
-  // ну ретерн и ретерн, че бухтеть-то
+// ну ретерн и ретерн
   return (
       <div className="container">
         <div>
-          <input type="text" value={result} autoFocus={true} tabIndex="0"/>
+          <input id="input" type="text" value={result} autoFocus={true} tabIndex="0" onChange={(event) => e(event.target.value)} />
         </div>
         <div className="keypad">
           {buttons.map((b) => <Button name={b} onClick={handleClick}/>)}
@@ -112,77 +128,5 @@ const App = () => {
       </div>
   );
 }
-
-// nodeJs - hehe - а вот и нет
-class node {
-  constructor(expression) {
-    this.left = null;
-    this.right = null;
-    this.op = null;
-    this.expression = expression;
-    this.isSimple = false;
-  }
-
-  // тут у нас типо парсер
-  parse() {
-    let position;
-    if (this.expression.indexOf('+') !== -1) {
-      this.op = '+';
-      position = this.expression.indexOf('+');
-    } else if (this.expression.indexOf('-') !== -1) {
-      this.op = '-';
-      position = this.expression.indexOf('-');
-    } else if (this.expression.indexOf('/') !== -1) {
-      this.op = '/';
-      position = this.expression.indexOf('/');
-    } else if (this.expression.indexOf('*') !== -1) {
-      this.op = '*';
-      position = this.expression.indexOf('*');
-    } else if (this.expression.indexOf('^') !== -1) {
-      this.op = '^';
-      position = this.expression.indexOf('^');
-    } else {
-      this.isSimple = true;
-    }
-    if (!this.isSimple) {
-      // да, тут костыль
-      if (position === 0 && this.op === '-') {
-        this.left = new node('0');
-        this.right = new node(this.expression.slice(1, this.expression.length));
-      } else {
-        this.left = new node(this.expression.slice(0, position));
-        this.right = new node(this.expression.slice(position + 1, this.expression.length));
-      }
-      this.left.parse();
-      this.right.parse();
-    }
-    return this.evaluate();
-  }
-
-  // как eval, но не eval - eval нельзя
-  evaluate() {
-    if (this.isSimple) return parseFloat(this.expression);
-    // не, ну а чо, нормальный switch
-    switch (this.op) {
-      case '+' :
-        return this.left.evaluate() + this.right.evaluate();
-        break;
-      case '*' :
-        return this.left.evaluate() * this.right.evaluate();
-        break;
-      case '/' :
-        return this.left.evaluate() / this.right.evaluate();
-        break;
-      case '^' :
-        return Math.pow(this.left.evaluate(), this.right.evaluate());
-        break;
-      case '-' :
-        return this.left.evaluate() - this.right.evaluate();
-        break;
-      default : console.log('Error : unknown operator');
-    }
-  }
-}
-
 
 export default App
---
+++ src/Node.js
@@ -0,0 +1,72 @@
+class Node {
+  constructor(expression) {
+    this.left = null;
+    this.right = null;
+    this.op = null;
+    this.expression = expression;
+    this.isSimple = false;
+  }
+
+  // тут у нас типо парсер
+  parse() {
+    let position;
+    if (this.expression.indexOf('+') !== -1) {
+      this.op = '+';
+      position = this.expression.indexOf('+');
+    } else if (this.expression.indexOf('-') !== -1) {
+      this.op = '-';
+      position = this.expression.indexOf('-');
+    } else if (this.expression.indexOf('*') !== -1) {
+      this.op = '*';
+      position = this.expression.indexOf('*');
+    } else if (this.expression.indexOf('%') !== -1) {
+      this.op = '%';
+      position = this.expression.indexOf('%');
+    } else if (this.expression.indexOf('^') !== -1) {
+      this.op = '^';
+      position = this.expression.indexOf('^');
+    } else {
+      this.isSimple = true;
+    }
+    if (!this.isSimple) {
+      // да, тут костыль
+      if (position === 0 && this.op === '-') {
+        this.left = new Node('0');
+        this.right = new Node(this.expression.slice(1, this.expression.length));
+      } else {
+        this.left = new Node(this.expression.slice(0, position));
+        this.right = new Node(this.expression.slice(position + 1, this.expression.length));
+      }
+      this.left.parse();
+      this.right.parse();
+    }
+    return this.evaluate();
+  }
+
+  // как eval, но не eval - eval нельзя
+  evaluate() {
+    if (this.isSimple) return parseFloat(this.expression);
+    // не, ну а чо, нормальный switch
+    switch (this.op) {
+      case '+' :
+        return this.left.evaluate() + this.right.evaluate();
+        break;
+      case '*' :
+        return this.left.evaluate() * this.right.evaluate();
+        break;
+      case '%' :
+        return this.left.evaluate() % this.right.evaluate();
+        break;
+      case '^' :
+        return Math.pow(this.left.evaluate(), this.right.evaluate());
+        break;
+      case '-' :
+        return this.left.evaluate() - this.right.evaluate();
+        break;
+      default :
+        console.log('Error : unknown operator');
+    }
+  }
+}
+
+export default Node

93b6c21d9c1cb26129ecd5765436306757cdfb20
Nikita Prokopyev <nikpro200125@gmail.com>
2021-11-14 01:05:34 +0300

    prettier applied

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import React, {useState} from "react";
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Node from "./Node";
 
 // константы
-const buttons = ['clear', '←', '^', '%', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-const manips = ['clear', '←', '^', '%', '*', '-', '=', '+'];
+const buttons = [
+  "clear",
+  "←",
+  "^",
+  "%",
+  "7",
+  "8",
+  "9",
+  "*",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
+const manips = ["clear", "←", "^", "%", "*", "-", "=", "+"];
 let tabs = 1;
 
-// Пожилая кнопка
 const Button = (props) => {
   if (manips.includes(props.name)) {
     return (
-        <button key={props.name}
-                className="highlight"
-                name={props.name}
-                onClick={props.onClick}
-                tabIndex={tabs++}>{props.name}
+        <button
+            key={props.name}
+            className="highlight"
+            name={props.name}
+            onClick={props.onClick}
+            tabIndex={tabs++}
+        >
+          {props.name}
         </button>
     );
   } else {
     return (
-        <button key={props.name}
-                name={props.name}
-                onClick={props.onClick}
-                tabIndex={tabs++}>{props.name}
+        <button
+            key={props.name}
+            name={props.name}
+            onClick={props.onClick}
+            tabIndex={tabs++}
+        >
+          {props.name}
         </button>
     );
   }
-}
+};
 
-// приложение вот
 const App = () => {
-  const [result, setResult] = useState('0');
+  const [result, setResult] = useState("0");
 
   const validMiddleWare = (value) =>
       value.match(/^[0-9./\-*+%^]+$/) &&
@@ -42,104 +66,111 @@ const App = () => {
 
   const keyboardEventHandler = (event) => {
     const key = event.key;
-    if (key === 'Escape') {
-      handleClick('clear');
-    } else if (key === 'Enter') {
-      handleClick('=');
+    if (key === "Escape") {
+      handleClick("clear");
+    } else if (key === "Enter") {
+      handleClick("=");
     }
-  }
+  };
 
   const evaluate = (value) => {
-    if (value.charAt(value.length - 1) === '=') {
+    if (value.charAt(value.length - 1) === "=") {
       value = value.slice(0, -1);
-      if (validMiddleWare(value) && !value.match(validExp) && !value.match(/%$|\+$|-$|\^$|\.$|\*$/)) {
+      if (
+          validMiddleWare(value) &&
+          !value.match(validExp) &&
+          !value.match(/%$|\+$|-$|\^$|\.$|\*$/)
+      ) {
         const v = new Node(value).parse();
-        setResult((v > Number.MAX_SAFE_INTEGER || v < Number.MIN_SAFE_INTEGER) ? 'Error' : v.toString());
+        setResult(
+            v > Number.MAX_SAFE_INTEGER || v < Number.MIN_SAFE_INTEGER
+                ? "Error"
+                : v.toString()
+        );
       }
     } else {
       if (validMiddleWare(value)) {
         if (!value.match(validExp)) {
           setResult(value);
-        } else if (value.match(/^0[0-9]+/) && !value.match(/\+0[0-9]+|-0[0-9]+|\*0[0-9]+|%0[0-9]+|\^0[0-9]+/)) {
+        } else if (
+            value.match(/^0[0-9]+/) &&
+            !value.match(/\+0[0-9]+|-0[0-9]+|\*0[0-9]+|%0[0-9]+|\^0[0-9]+/)
+        ) {
           setResult(value.slice(1));
         }
-      } else if (value === '') {
-        setResult('0');
+      } else if (value === "") {
+        setResult("0");
       }
     }
-  }
+  };
 
-// Проверка, что нет более одной точки в числе
+  // Проверка, что нет более одной точки в числе
   const checkNumberOfDots = (value) => {
     let counter = 0;
     for (let i = value.length - 1; i > -1 && counter < 2; i--) {
       if (value[i].match(/^[+\-%*^]$/)) {
         counter = 0;
       }
-      if (value[i] === '.') {
+      if (value[i] === ".") {
         counter++;
       }
     }
     return counter < 2;
-  }
+  };
 
-// обработка кнопок
+  // обработка кнопок
   const handleClick = (clickSymbol) => {
     if (!clickSymbol.match(/^[0-9]$/)) {
       switch (clickSymbol) {
-        case 'clear' :
-          setResult('0');
+        case "clear":
+          setResult("0");
           break;
-        case '←' :
+        case "←":
           if (result.length > 1) {
             setResult(result.slice(0, -1));
           } else {
-            setResult('0');
+            setResult("0");
           }
           break;
-        case '=' :
-          evaluate(result + '=');
+        case "=":
+          evaluate(result + "=");
           break;
-        case '-' :
-          if (result === '0') {
-            setResult('-');
+        case "-":
+          if (result === "0") {
+            setResult("-");
             break;
           }
           // eslint-disable-next-line
-        default :
+        default:
           evaluate(result + clickSymbol);
       }
-    } else if (result === '0') {
+    } else if (result === "0") {
       setResult(clickSymbol);
     } else {
       setResult(result + clickSymbol);
     }
-  }
+  };
 
   return (
       <div className="container">
         <div>
-          <input id="input"
-                 type="text"
-                 value={result}
-                 autoFocus={true}
-                 tabIndex="0"
-                 onChange={(event) => evaluate(event.target.value)}
-                 onKeyUp={keyboardEventHandler}/>
+          <input
+              id="input"
+              type="text"
+              value={result}
+              autoFocus={true}
+              tabIndex="0"
+              onChange={(event) => evaluate(event.target.value)}
+              onKeyUp={keyboardEventHandler}
+          />
         </div>
         <div className="keypad">
-          {
-            buttons.map(
-                (b) =>
-                    <Button
-                        name={b}
-                        onClick={(e) => handleClick(e.target.name)}
-                    />
-            )
-          }
+          {buttons.map((b) => (
+              <Button name={b} onClick={(e) => handleClick(e.target.name)} />
+          ))}
         </div>
       </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/Node.js
+++ src/Node.js
@@ -10,32 +10,34 @@ class Node {
   // тут у нас типо парсер
   parse() {
     let position;
-    if (this.expression.indexOf('+') !== -1) {
-      this.op = '+';
-      position = this.expression.indexOf('+');
-    } else if (this.expression.indexOf('-') !== -1) {
-      this.op = '-';
-      position = this.expression.indexOf('-');
-    } else if (this.expression.indexOf('*') !== -1) {
-      this.op = '*';
-      position = this.expression.indexOf('*');
-    } else if (this.expression.indexOf('%') !== -1) {
-      this.op = '%';
-      position = this.expression.indexOf('%');
-    } else if (this.expression.indexOf('^') !== -1) {
-      this.op = '^';
-      position = this.expression.indexOf('^');
+    if (this.expression.indexOf("+") !== -1) {
+      this.op = "+";
+      position = this.expression.indexOf("+");
+    } else if (this.expression.indexOf("-") !== -1) {
+      this.op = "-";
+      position = this.expression.indexOf("-");
+    } else if (this.expression.indexOf("*") !== -1) {
+      this.op = "*";
+      position = this.expression.indexOf("*");
+    } else if (this.expression.indexOf("%") !== -1) {
+      this.op = "%";
+      position = this.expression.indexOf("%");
+    } else if (this.expression.indexOf("^") !== -1) {
+      this.op = "^";
+      position = this.expression.indexOf("^");
     } else {
       this.isSimple = true;
     }
     if (!this.isSimple) {
       // да, тут костыль
-      if (position === 0 && this.op === '-') {
-        this.left = new Node('0');
+      if (position === 0 && this.op === "-") {
+        this.left = new Node("0");
         this.right = new Node(this.expression.slice(1, this.expression.length));
       } else {
         this.left = new Node(this.expression.slice(0, position));
-        this.right = new Node(this.expression.slice(position + 1, this.expression.length));
+        this.right = new Node(
+            this.expression.slice(position + 1, this.expression.length)
+        );
       }
       this.left.parse();
       this.right.parse();
@@ -48,20 +50,20 @@ class Node {
     if (this.isSimple) return parseFloat(this.expression);
     // не, ну а чо, нормальный switch
     switch (this.op) {
-      case '+' :
+      case "+":
         return this.left.evaluate() + this.right.evaluate();
-      case '*' :
+      case "*":
         return this.left.evaluate() * this.right.evaluate();
-      case '%' :
+      case "%":
         return this.left.evaluate() % this.right.evaluate();
-      case '^' :
+      case "^":
         return Math.pow(this.left.evaluate(), this.right.evaluate());
-      case '-' :
+      case "-":
         return this.left.evaluate() - this.right.evaluate();
-      default :
-        console.log('Error : unknown operator');
+      default:
+        console.log("Error : unknown operator");
     }
   }
 }
 
-export default Node
+export default Node;
